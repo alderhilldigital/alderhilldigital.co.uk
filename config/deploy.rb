28d74d71c394a3dca5be6ef0fe33a25fbfcca1ac
Alan Stirling <alanstirling@Alan-Stirlings-MacBook-Pro.local>
2017-02-14 21:07:54 +0000

    unicorn tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,19 +46,37 @@ task :setup do
   # command %{rbenv install 2.3.0}
 end
 
-namespace :unicorn do
-  set :unicorn_pid, "/home/unicorn/pids/unicorn.pid"
+set :app_path, "/var/app/#{application_name}/current"
+set :shared_path, "/var/app/#{application_name}/shared"
 
+namespace :unicorn do
+  set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
+  set :start_unicorn, %{
+    cd #{app_path}
+    bundle exec unicorn -c #{app_path}/config/unicorn.rb -E #{rails_env} -D
+  }
+
+  #                                                                    Start task
+# ------------------------------------------------------------------------------
   desc "Start unicorn"
   task :start => :environment do
-    command %{service unicorn_alderhilldigital_co_uk start}
+    queue 'echo "-----> Start Unicorn"'
+    queue! start_unicorn
   end
 
+#                                                                     Stop task
+# ------------------------------------------------------------------------------
   desc "Stop unicorn"
   task :stop do
-    command %{service unicorn_alderhilldigital_co_uk stop}
+    queue 'echo "-----> Stop Unicorn"'
+    queue! %{
+      test -s "#{unicorn_pid}" && kill -QUIT `cat "#{unicorn_pid}"` && echo "Stop Ok" && exit 0
+      echo >&2 "Not running"
+    }
   end
 
+#                                                                  Restart task
+# ------------------------------------------------------------------------------
   desc "Restart unicorn using 'upgrade'"
   task :restart => :environment do
     invoke 'unicorn:stop'
