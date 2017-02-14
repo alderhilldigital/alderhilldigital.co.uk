5d81e73a2d577b531a20d0b768968de2a181ed1d
Alan Stirling <alanstirling@AlanStilingsMBP.home>
2017-02-14 12:22:43 +0000

    add mina rbenv include

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
+require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
 # require 'mina/rvm'    # for rvm support. (https://rvm.io)
 
 # Basic settings:
