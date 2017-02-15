2fd5331423c951afbcb7964acceb6ff12eb5c994
Alan Stirling <alanstirling@Alan-Stirlings-MacBook-Pro.local>
2017-02-15 10:22:06 +0000

    format contact email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,9 @@ class UserMailer < ApplicationMailer
 
   def contact_email(params)
     @name  = params[:name]
-    mail(to: 'info@alderhilldigital.co.uk', subject: 'New website contact')
+    @email  = params[:email]
+    @message  = params[:message]
+    mail(to: 'info@astirling.co.uk', subject: 'New website contact')
   end
 
 end
