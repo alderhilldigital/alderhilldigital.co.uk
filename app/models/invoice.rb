65e44474f7d942818aef1644beab24ae7d357425
Alan Stirling <flashharry82@yahoo.com>
2017-05-11 18:22:00 +0100

    dont require address line 2

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,5 @@
 class Invoice < ActiveRecord::Base
 
-  validates_presence_of :organisation_name, :contact_person, :address_line_1, :address_line_2, :town, :county, :postcode, :telephone_no, :name, :email
+  validates_presence_of :organisation_name, :contact_person, :address_line_1, :town, :county, :postcode, :telephone_no, :name, :email
 
 end
